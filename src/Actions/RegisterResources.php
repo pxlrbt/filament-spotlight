28f7e06fc97a221083d3671b088ba33c3fcaf3b8
Dennis Koch <info@pixelarbeit.de>
2024-02-22 08:00:44 +0100

    #39 `shouldRegisterSpotlight()` for resources and resource pages

--- src/Actions/RegisterResources.php
+++ src/Actions/RegisterResources.php
@@ -13,10 +13,20 @@ class RegisterResources
     {
         $resources = $panel->getResources();
 
+
+
         foreach ($resources as $resource) {
+            if (method_exists($resource, 'shouldRegisterSpotlight') && $resource::shouldRegisterSpotlight() === false) {
+                continue;
+            }
+
             $pages = $resource::getPages();
 
             foreach ($pages as $key => $page) {
+                if (method_exists($page->getPage(), 'shouldRegisterSpotlight') && $page->getPage()::shouldRegisterSpotlight() === false) {
+                    continue;
+                }
+
                 /**
                  * @var PageRegistration $page
                  */
